d91ea038954818c788a1c71041737a4d624810ea
coshaugh <coshaugh1@gmail.com>
2016-03-29 00:53:12 -0700

    removing dated files, added style & favicon

--- userData.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from google.appengine.ext import db
-
-class User(db.Model):
-	name = db.StringProperty(required=True)
-	password = db.StringProperty()
-	email = db.StringProperty()
-
