0b1302d4c089fda2c626b7c7cf58a684fe8e681c
samDieterle <diete002@cougars.CSUSM.edu>
2016-05-09 20:11:56 -0700

    Update mainwindow.cpp
    Only needs to fix the volume/duration/song name

--- musicPlayer/mainwindow.cpp
+++ musicPlayer/mainwindow.cpp
@@ -7,6 +7,9 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     _player = NULL;
+    //connect(_player, &Player::positionChanged, this, &MainWindow::on_positionChanged);
+    //connect(_player, &Player::durationChanged, this, &MainWindow::on_durationChanged);
+
 }
 
 MainWindow::~MainWindow()
@@ -28,4 +31,35 @@ void MainWindow::on_PauseButton_clicked()
 void MainWindow::on_PlayButton_clicked()
 {
     _player->play();
+}
+
+void MainWindow::on_NextButton_clicked()
+{
+    _player->next();
+}
+
+void MainWindow::on_PreviousButton_clicked()
+{
+    _player->previous();
+}
+
+void MainWindow::on_VolumeSlider_sliderMoved(int position)
+{
+    //_player->setVolume(position);
+}
+
+void MainWindow::on_ProgressSlider_sliderMoved(int position)
+{
+    //_player->setPosition(position);
+}
+
+
+void MainWindow::on_durationChanged(qint64 position)
+{
+    //ui->Volume->setMaximum(position);
+}
+
+void MainWindow::on_positionChanged(qint64 position)
+{
+    //ui->Duration->setValue(position);
 }
